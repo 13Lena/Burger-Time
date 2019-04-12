d247b6e864e46e3613d24209dd5b8f52106e7b83
Lena <lena@system76-pc.localdomain>
2019-04-12 16:37:57 -0600

    file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('BBQ Burger');
+INSERT INTO burgers (name) VALUES ('Denver Burger');
+INSERT INTO burgers (name) VALUES ('Tuna Burger');
+INSERT INTO burgers (name) VALUES ('Pizza Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Mushroon Burger');
